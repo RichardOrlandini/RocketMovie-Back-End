d07a2a837230ac13f50e8d03c1fb106a7c87c4df
RichardOlrandini <richardorlandini09@gmail.com>
2023-02-15 09:13:19 -0300

    novos testes de usuario

---
+++ Projeto RocketMovie/src/repositories/UserRepositoryInMemory.js
@@ -0,0 +1,24 @@
+
+
+class UserRepositoryInMemory {
+    users = [];
+    
+
+    async create({ name, email, password}){
+        const user = {
+            id: Math.floor(Math.random() * 1000) + 1,
+            name,
+            email,
+            password
+        };
+
+        this.users.push(user);
+
+        return user;
+    }
+
+    async findByEmail(email){
+        return this.users.find(user => user.email === email);
+    }
+}
+module.exports = UserRepositoryInMemory;
--- Projeto RocketMovie/src/services/userCreateServices.spec.js
+++ Projeto RocketMovie/src/services/userCreateServices.spec.js
@@ -1,14 +1,45 @@
 const UserCreateServices = require("./UserCreateServices");
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
+const AppError = require("../utils/AppError");
 
-it("user should be create", () => {
-    const user = {
-        name: "User Test",
+
+describe("UserCreateService", () => {
+    let userRepositoryInMemory = null;
+    let userCreateServices = null;
+
+    beforeEach(() => {
+        userRepositoryInMemory = new UserRepositoryInMemory();
+        userCreateServices = new UserCreateServices(userRepositoryInMemory);
+    })
+    
+    it("user should be create", async () => {  
+        const user = {
+            name: "User Test",
+            email: "user@test.com",
+            password: "123"
+        };
+    
+
+        const userCreated = await userCreateServices.execute(user);
+    
+        expect(userCreated).toHaveProperty("id");
+    });
+
+    it("user not should be create with exists email", async () => {
+      const user1= {
+        name: "User test",
         email: "user@test.com",
         password: "123"
-    };
+      };
 
-    const userCreateServices = new UserCreateServices;
-    const userCreated = userCreateServices.execute(user);
+      const user2= {
+        name: "User test 2",
+        email: "user@test.com",
+        password: "546"
+      };
 
-    expect(userCreated).toHaveProperty("id");
+
+      await userCreateServices.execute(user1);
+      await expect(userCreateServices.execute(user2)).rejects.toEqual(new AppError("Este email já  está em uso!"))
+    });
 });
